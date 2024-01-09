3abac530d0ef958be363236e2238fea79bd8cd62
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-09 18:04:29 +0530

    Added a turret

---
+++ Refactor(A TD Game)/Assets/Scripts/Turret.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Turret : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform turretRotatePoint;
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attributes")]
+    [SerializeField] private float targetingRange = 4f;
+
+    private Transform target;
+
+    private void Update()
+    {
+        if(target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if(hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle - 90f));
+        turretRotatePoint.rotation = targetRotation;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+
+}
