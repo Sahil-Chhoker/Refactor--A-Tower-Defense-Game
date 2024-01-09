6f12963d950f47add8859c7a620a8222a07df52d
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-09 19:08:40 +0530

    Remade The Map

---
+++ Refactor(A TD Game)/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager main;
+
+    [Header("References")]
+    [SerializeField] private GameObject[] towerPrefabs;
+
+    private int selectedTower = 0;
+
+    private void Awake()
+    {
+        main = this;
+    }
+
+    public GameObject GetSelectedTower()
+    {
+        return towerPrefabs[selectedTower];
+    }
+}
