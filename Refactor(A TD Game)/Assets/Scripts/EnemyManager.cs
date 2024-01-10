a8f7501cf2d2f0a991b784f372373db758c3c7ce
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-10 11:18:24 +0530

    Added a SLowmo Turret

--- Refactor(A TD Game)/Assets/Scripts/EnemyManager.cs
+++ Refactor(A TD Game)/Assets/Scripts/EnemyManager.cs
@@ -12,9 +12,11 @@ public class EnemyManager : MonoBehaviour
 
     private Transform target;
     private int pathIndex = 0;
+    private float baseSpeed;
 
     private void Start()
     {
+        baseSpeed = moveSpeed;
         target = LevelManager.main.path[pathIndex];
     }
 
@@ -42,5 +44,15 @@ public class EnemyManager : MonoBehaviour
         Vector2 direction = (target.position - transform.position).normalized;
 
         rb.velocity = direction * moveSpeed;
+    }
+
+    public void UpdateSpeed(float newSpeed)
+    {
+        moveSpeed = newSpeed;
+    }
+
+    public void ResetSpeed()
+    {
+        moveSpeed = baseSpeed;
     }
 }
---
+++ Refactor(A TD Game)/Assets/Scripts/SlowmoTurret.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+public class SlowmoTurret : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private LayerMask enemyMask;
+    
+    [Header("Attributes")]
+    [SerializeField] private float targetingRange = 4f;
+    [SerializeField] private float freezeTime = 1.5f;
+    [SerializeField] private float aps = 0.25f; //Attacks Per Second
+
+    private float timeUntilFire;
+
+    private void Update()
+    {
+        timeUntilFire += Time.deltaTime;
+
+        if(timeUntilFire >= 1f / aps)
+        {
+            FreezeEnemies();
+            timeUntilFire = 0.0f;
+        }
+        
+    }
+
+    private void FreezeEnemies()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if(hits.Length > 0)
+        {
+            for(int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit2D hit = hits[i];
+
+                EnemyManager em = hit.transform.GetComponent< EnemyManager>();
+                em.UpdateSpeed(0.5f);
+
+                StartCoroutine(ResetEnemySpeed(em));
+            }
+        }
+    }
+
+
+    private IEnumerator ResetEnemySpeed(EnemyManager em)
+    {
+        yield return new WaitForSeconds(freezeTime);
+
+        em.ResetSpeed();
+    }
+
+    
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+}
